e6555880b50f9d95ee7a9d61b07f2cea90ef692a
Jacob Berman <jacobberman1995@gmail.com>
2018-08-12 18:16:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,8 @@ def adding_matz
      }
      programmer_hash[:yukihiro_matsumoto] ={}
      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-    # programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp, C"]
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp, C"]
+    programmer_hash
 
      
 
